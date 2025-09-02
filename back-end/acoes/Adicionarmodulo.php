a5ced98a4c53e725ba820d77b6c55c8c0efe2e19
guhbarros99 <gu01stavo@gmail.com>
2025-09-01 21:57:54 -0300

    Funcionou, mas session está dando erro ainda

--- back-end/acoes/Adicionarmodulo.php
+++ back-end/acoes/Adicionarmodulo.php
@@ -9,52 +9,49 @@
     FOREIGN KEY (id_empresa) REFERENCES empresa(id)
 ); -->
 <?php
-require_once __DIR__ . '/../dao/EmpresaDAO.php';
-require_once __DIR__ . '/../model/Empresa.php';
+session_start();
+ 
 require_once __DIR__ . '/../dao/CampoDAO.php';
-require_once __DIR__ . '/../model/Campo.php';   
-
+require_once __DIR__ . '/../model/Campo.php';
+ 
 $campoDAO = new CampoDAO();
-
-$campo = $campoDAO->buscarCampoPorId($_GET['id_campo']);
-
-
-
-if (!$campo) {
-    die("Campo não encontrado.");
+$campo = $campoDAO->buscarCampoPorId($_GET['id_campo'] ?? 0);
+ 
+if (!isset($_SESSION['token'])) {
+    header('Location: ../cadastro/Cadastro.php'); // Redirecionar para a página de cadastro se não estiver autenticado
+    exit();
 }
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = $_POST['nome'] ?? '';
-    $id = $_POST['id'] ?? '';
+    $id_campo = $_POST['id_campo'] ?? '';
     $id_empresa = $_POST['id_empresa'] ?? '';
-
-    if ($nome && $id && $id_empresa) {
-
-    
+ 
+    if ($nome && $id_campo && $id_empresa) {
+ 
+   
         require_once __DIR__ . '/../model/Modulo.php';
         require_once __DIR__ . '/../dao/ModuloDAO.php';
         $moduloDAO = new ModuloDAO();
-        $modulo = new Modulo(null, $nome, (int)$id, (int)$id_empresa);
+        $modulo = new Modulo(null, $nome, (int)$id_campo, (int)$id_empresa);
         if($moduloDAO->adicionarModulo($modulo)){
-            header('Location: ../home.php?id='.$id);
+            header('Location: ../home.php?id='.$id_campo);
             exit();
         } else {
             echo "Erro ao cadastrar módulo.";
         }
-
+ 
        
     }else {
         echo "Todos os campos são obrigatórios.";
     }
-
-
-
+ 
+ 
+ 
 }
-
-
-
-
+ 
+ 
+ 
+ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,20 +62,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../../css/login.css">
 </head>
 <body>
-    
+   
     <form action="Adicionarmodulo.php" method="POST">
         <h1>Adicionar <?= $campo->getNome(); ?></h1>
-        <label for="nome">Nome:</label>
+        <label for="nome">Nome do Modulo:</label>
         <input type="text" id="nome" name="nome" required><br><br>
-
+ 
        
         <input hidden type="number" id="id_campo" name="id_campo" value="<?= $_GET['id_campo'] ?>"><br><br>
-
+ 
         <input hidden name="id_empresa" value="<?=$_SESSION['id_empresa']; ?>">
-
+ 
         <input type="submit" value="Adicionar Modulo">
-
+ 
     </form>
-
+ 
 </body>
 </html>
+ 
